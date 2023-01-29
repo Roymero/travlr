261b4ebdbd2dda468b9616079d40d9bb0d6b0f44
Roy Romero <Romerorr94@gmail.com>
2023-01-28 19:36:36 -0500

    Converted static HTML in the travel section to reusable json templates

--- app_server/controllers/travel.js
+++ app_server/controllers/travel.js
@@ -1,9 +1,15 @@
+/* Reading trip data */ 
+
+var fs = require('fs');
+
+var trips = JSON.parse(fs.readFileSync('./data/trips.json', 'utf8'));
+
 
 /* GET travel view */
 
 const travel = (req, res) => {
     
-    res.render('travel', { title: 'Travlr Getaways' });
+    res.render('travel', { title: 'Travlr Getaways', trips });
 
 };
 
